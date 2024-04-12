db42be88a19e9cb56d0bf4458962280e72eb9f79
satish <satish@dijitru.com>
2024-04-12 18:10:47 -0400

    initial set up.

---
+++ edi-csv-generator/src/main/java/com/mango/edi/App.java
@@ -0,0 +1,13 @@
+package com.mango.edi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
